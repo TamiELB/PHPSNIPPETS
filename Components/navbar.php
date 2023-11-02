b075833d0e056e8557532cda5f81bc25c43c7432
Tami Bouazzati <tami.dev@outlook.com>
2023-11-02 13:57:54 +0100

    first commit

---
+++ Components/navbar.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="/Zeilschool/CSS/navbar.css">
+
+</head>
+    <body>
+    <header>
+            <div class="left">
+                <a href="" class="title">ZeilSchool</a>
+
+                <a class="toggle-button" onclick="ToggleMenu()">
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                </a>    
+            </div>
+            <nav id="nav">
+                <ul>
+                    <li><a href="#">Login</a></li>
+                    <li><a href="#">Main</a></li>
+                    <li><a href="#">Profile</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        
+
+        <script src="/Zeilschool/JS/navbar.js"></script>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <title>ZeilSchool</title>
+        <link rel="stylesheet" type="text/css" href="CSS/main.css"> 
+    </head>
+        <body>
+            <?php include './Components/navbar.php'; ?>
+
+        </body>
+    </html>
